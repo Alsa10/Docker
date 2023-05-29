b8e480b68be15aa2d1be6e37f9b9c115c765a75b
Alsa10 <alok.devops12@gmail.com>
2023-05-29 17:36:40 +0530

    functions

---
+++ 02-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function sample() {
+
+    echo hello
+
+}
+
+# or
+sample() {
+    echo hello
+}
+
+sample
+
+## Good Morning Messages
+
+Welcome_message() {
+
+    echo "Hello, Good Morning"
+    echo "Welcome to Devops"
+
+} 
+
+Welcome_message
+
+}
